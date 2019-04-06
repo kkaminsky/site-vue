06474235db197d1994537b309b209ee62d11b374
kkaminsky <mr.kkaminsky@list.ru>
2019-04-06 12:49:37 +0500

    First example

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <v-navigation-drawer
+                persistent
+                :mini-variant="miniVariant"
+                :clipped="clipped"
+                v-model="drawer"
+                enable-resize-watcher
+                fixed
+                app
+        >
+            <v-list two-line class="pa-0">
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
+                        <v-icon>account_circle</v-icon>
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{name1}}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+
+                <v-list>
+                    <v-list-tile v-for="item in menuItems" :to="item.route">
+                        <v-list-tile-action>
+                            <v-icon v-html="item.icon"></v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-toolbar
+                app
+                :clipped-left="clipped"
+        >
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-btn icon @click.stop="miniVariant = !miniVariant">
+                <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+            </v-btn>
+            <v-btn icon @click.stop="clipped = !clipped">
+                <v-icon>web</v-icon>
+            </v-btn>
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        data () {
+            return {
+                clipped: false,
+                drawer: true,
+                fixed: false,
+                miniVariant: true,
+                right: true,
+                rightDrawer: false,
+                title: 'Логистика 2.0'
+            }
+        },
+        computed: {
+            name1() {
+                return "ff"
+            },
+            roles() {
+                return "ff"
+            },
+            menuItems() {
+                return [
+                    {
+                        icon: 'book',
+                        title: 'Домашняя страница',
+                        route: '/'
+                    },
+                    {
+                        icon: 'description',
+                        title: 'События',
+                        route: 'events',
+                    }
+
+                ]
+            },
+            name: 'App'
+        }}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,25 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Events from './views/Events.vue'
+
+
+
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
+      meta: { requiresAuth: true, roles: ['user'] },
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'/events',
+      name:'events',
+      meta: { requiresAuth: true, roles: ['user'] },
+      component: Events
     }
-  ]
+  ],
+  mode:'history'
 })
+
+
+
+
+export default router
