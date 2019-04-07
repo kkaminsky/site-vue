7a2be148d44a00cb0f3be7bc4179543cfad92b4e
krokodainer9090 <37234216+krokodainer9090@users.noreply.github.com>
2019-04-07 12:49:25 +0500

    CheckPoint2
    jytjyt

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Events from './views/Events.vue'
+import Cabinet from './views/Cabinet.vue'
 
 
 
@@ -21,7 +22,13 @@ const router = new Router({
       name:'events',
       meta: { requiresAuth: true, roles: ['user'] },
       component: Events
-    }
+    },
+    {
+      path:'/cabinet',
+      name:'cabinet',
+      meta: { requiresAuth: true, roles: ['user'] },
+      component: Cabinet
+    },
   ],
   mode:'history'
 })
---
+++ src/views/Cabinet.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <cabinet-page></cabinet-page>
+    </div>
+</template>
+
+<script>
+    import CabinetPage from '../components/CabinetPage'
+
+    export default {
+        components:{
+            CabinetPage
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
