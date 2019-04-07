869341868ee13eb31bfe7ea2f4c457f8d75cf58e
kkaminsky <mr.kkaminsky@list.ru>
2019-04-07 22:09:50 +0500

    commit before merge

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,28 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+      <app-header></app-header>
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
+import AppHeader from './components/AppHeader'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+  components:{
+    AppHeader
   }
 }
 </script>
+
+
+<style lang="scss">
+  @import "./styles/mixins.scss";
+
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    text-align: center;
+  }
+</style>
