f0701787ca463e66f324650cfdab73c7ac0ac946
krokodainer9090 <37234216+krokodainer9090@users.noreply.github.com>
2019-04-28 16:26:16 +0500

    img

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -8,7 +8,7 @@
       column
       justify-center
     >
-      <h1 class="display-3 font-weight-thin mb-3">EventsMakers</h1>
+      <h1 class="display-3 font-weight-thin mb-3">The Event</h1>
       <h3 class="subheading text-xs-center">Мы отбираем только захватывающие мероприятия</h3>
     </v-layout>
   </v-parallax>
